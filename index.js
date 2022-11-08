9cfc406926da35bd28376b0965b01cb3e87f0026
NabilNewaz <nabilnewaz@gmail.com>
2022-11-09 03:29:31 +0600

    reviews data get function added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('lawyerReview').collection('services');
+        const reviewsCollection = client.db('lawyerReview').collection('reviews');
 
         app.get('/services', async (req, res) => {
             const dataSize = req.query.datasize;
@@ -30,6 +31,14 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        })
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { service_id: id };
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         })
     }
     finally {
