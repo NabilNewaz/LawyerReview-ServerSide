2760ee1edb054062033232c280340aa4d4c65988
NabilNewaz <nabilnewaz@gmail.com>
2022-11-10 02:27:59 +0600

    review delete function added

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
